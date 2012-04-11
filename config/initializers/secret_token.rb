555313272495b9ddfbb9f58e126038744c296439
al-Amjad Tawfiq Isstaif <isstaif@gmail.com>
2012-04-11 21:18:50 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Openhealthatlas::Application.config.secret_token = 'ddbeb61453bd1d00dceeffcdf6e291ec3a66fec52ecbcd4927f0ac51da27edb1d5dca8f1b89f9d36e75c79ae2b81bb8b167472cc2c82386fa9cd2edbfb761386'
